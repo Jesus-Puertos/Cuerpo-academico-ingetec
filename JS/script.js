b15202a98f160348a935adf84ad76de555d0ea57
Jesus Alberto Rodriguez Puertos <130002671+Jesus-Puertos@users.noreply.github.com>
2023-11-04 02:42:31 -0600

    Modo oscuro con local storage

--- JS/script.js
+++ JS/script.js
@@ -6,13 +6,29 @@ $("#menu-close").click(function () {
 });
 
 //Switch
-const swith = document.querySelector(".switch");
+// Obtén la referencia al elemento HTML con la clase "switch"
+const switchElement = document.querySelector(".switch");
 
-swith.addEventListener("click", (e) => {
-  // toggle agrega una clase o quita una clase
-  swith.classList.toggle("active");
+// Agrega un evento de escucha al hacer clic en el interruptor
+switchElement.addEventListener("click", (e) => {
+  // Cambia la clase "active" del interruptor y del body
+  switchElement.classList.toggle("active");
   document.body.classList.toggle("active");
+
+  // Al hacer clic en el interruptor, guarda el estado en localStorage
+  if (document.body.classList.contains("active")) {
+    localStorage.setItem("activeMode", "enabled");
+  } else {
+    localStorage.setItem("activeMode", "disabled");
+  }
 });
+
+// Comprueba si el modo activo estaba habilitado previamente en localStorage
+if (localStorage.getItem("activeMode") === "enabled") {
+  // Si estaba habilitado, establece la clase "active" en el body y el interruptor
+  switchElement.classList.add("active");
+  document.body.classList.add("active");
+}
 
 //Efecto de animacion de scroll
 const observer = new IntersectionObserver((entries) => {
