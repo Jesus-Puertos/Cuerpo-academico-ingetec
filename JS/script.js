e69c455c24f59e99a4e39dbd624025cbae4c85ec
Jesus Alberto Rodriguez Puertos <130002671+Jesus-Puertos@users.noreply.github.com>
2023-11-04 23:48:03 -0600

    Agregue un preload a el sitio web

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
+/**
+ * PRELOAD
+ *
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+//Para boton hamburguesa
 $("#menu-btn").click(function () {
   $("nav .navigation ul").addClass("active");
 });
